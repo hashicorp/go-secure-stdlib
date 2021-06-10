412fa00923a8b6b7707d1c8b1f1b11f80de0b951
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2021-06-10 16:25:29 -0500

    Add ParsePath function to configutil (#2)

---
+++ configutil/parsepath_test.go
@@ -0,0 +1,79 @@
+package configutil
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParsePath(t *testing.T) {
+	t.Parallel()
+
+	file, err := os.CreateTemp("", "")
+	require.NoError(t, err)
+	_, err = file.WriteString("foo")
+	require.NoError(t, err)
+	require.NoError(t, file.Close())
+	defer os.Remove(file.Name())
+
+	require.NoError(t, os.Setenv("PATHTEST", "bar"))
+
+	cases := []struct {
+		name             string
+		inPath           string
+		outStr           string
+		notAUrl          bool
+		expErrorContains string
+	}{
+		{
+			name:   "file",
+			inPath: fmt.Sprintf("file://%s", file.Name()),
+			outStr: "foo",
+		},
+		{
+			name:   "env",
+			inPath: "env://PATHTEST",
+			outStr: "bar",
+		},
+		{
+			name:   "plain",
+			inPath: "zipzap",
+			outStr: "zipzap",
+		},
+		{
+			name:             "no file",
+			inPath:           "file:///dev/nullface",
+			outStr:           "file:///dev/nullface",
+			expErrorContains: "no such file or directory",
+		},
+		{
+			name:    "not a url",
+			inPath:  "http://" + string([]byte{0x00}),
+			outStr:  "http://" + string([]byte{0x00}),
+			notAUrl: true,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert, require := assert.New(t), require.New(t)
+			out, err := ParsePath(tt.inPath)
+			if tt.expErrorContains != "" {
+				require.Error(err)
+				assert.Contains(err.Error(), tt.expErrorContains)
+				return
+			}
+			if tt.notAUrl {
+				require.Error(err)
+				assert.True(errors.Is(err, ErrNotAUrl))
+				assert.Equal(tt.inPath, out)
+				return
+			}
+			require.NoError(err)
+			assert.Equal(tt.outStr, out)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/mitchellh/mapstructure v1.3.3
 	github.com/prometheus/client_golang v1.7.1
 	github.com/prometheus/common v0.11.1
+	github.com/stretchr/testify v1.5.1
 	google.golang.org/api v0.29.0
 	google.golang.org/protobuf v1.24.0
 )
