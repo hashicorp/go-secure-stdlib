1e6b8d7bb1e0815462b523c8d2a6734aa0837f80
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2022-06-28 10:31:15 -0700

    fix: Make testplugin functional
    The testplugin as previously implemented didn't
    support the Encrypt or Decrypt methods,
    since it did not have an AEAD cipher configured.
    This change configures an AES cipher with
    a hardcoded key, so that we can do Encrypt
    and Decrypt operations in tests.

--- configutil/testplugins/aead/main.go
+++ configutil/testplugins/aead/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 	"os"
 
@@ -9,7 +11,19 @@ import (
 )
 
 func main() {
-	if err := gkwp.ServePlugin(aead.NewWrapper()); err != nil {
+	block, err := aes.NewCipher([]byte("1234567890123456"))
+	if err != nil {
+		fmt.Println("Error creating AES block", err)
+		os.Exit(1)
+	}
+	aeadCipher, err := cipher.NewGCM(block)
+	if err != nil {
+		fmt.Println("Error creating GCM cipher", err)
+		os.Exit(1)
+	}
+	wrapper := aead.NewWrapper()
+	wrapper.SetAead(aeadCipher)
+	if err := gkwp.ServePlugin(wrapper); err != nil {
 		fmt.Println("Error serving plugin", err)
 		os.Exit(1)
 	}
