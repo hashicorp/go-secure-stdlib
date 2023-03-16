d9ba0aacc88a51e8621d910dd86248d1ed0ecaba
Scott G. Miller <smiller@hashicorp.com>
2023-03-16 12:57:03 -0500

    Unit tests + bug fixes

--- functional/memo.go
+++ functional/memo.go
@@ -10,8 +10,8 @@ type MemoizationWithContextFunc[T any] func(ctx context.Context) (T, error)
 
 func MemoizeOnce[T any](f MemoizationFunc[T]) MemoizationFunc[T] {
 	var once sync.Once
+	var rv T
 	return func() T {
-		var rv T
 
 		once.Do(func() {
 			rv = f()
@@ -27,7 +27,7 @@ func MemoizeOnceWithContext[T any](f MemoizationWithContextFunc[T]) MemoizationW
 	return func(ctx context.Context) (T, error) {
 		once.Do(func() {
 			lrv, lerr := f(ctx)
-			if err != nil {
+			if lerr != nil {
 				err = lerr
 			} else {
 				rv = lrv
