0b1cdcd44841190f824dad60675aea05e527a01c
Tom Proctor <tomhjp@users.noreply.github.com>
2023-08-21 17:50:13 +0100

    Implement ReattachFunc

---
+++ plugincontainer/container_reattach.go
@@ -0,0 +1,29 @@
+package plugincontainer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/client"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin/runner"
+)
+
+func ReattachFunc(logger hclog.Logger, id, hostSocketDir string) (runner.AttachedRunner, error) {
+	client, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = client.ContainerInspect(context.Background(), id)
+	if err != nil {
+		return nil, fmt.Errorf("container with ID %s not found: %w", id, err)
+	}
+
+	return &ContainerRunner{
+		dockerClient:  client,
+		logger:        logger,
+		id:            id,
+		hostSocketDir: hostSocketDir,
+	}, nil
+}
