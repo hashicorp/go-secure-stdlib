7087d35f9f407748fe16bbd9da72c8ca3ac5c435
Scott Miller <sgmiller@gmail.com>
2024-08-02 13:33:25 -0500

    Update httputil/cert_ext_tripper.go
    Co-authored-by: Jeff Mitchell <jeffrey.mitchell@gmail.com>

--- httputil/cert_ext_tripper.go
+++ httputil/cert_ext_tripper.go
@@ -22,7 +22,7 @@ type ignoreExtensionsRoundTripper struct {
 }
 
 // NewIgnoreUnhandledExtensionsRoundTripper creates a RoundTripper that may be used in an HTTP client which will
-// ignore the provided extensions if present unhandled on a certificate.  If base is nil, the default RoundTripper is used.
+// ignore the provided extensions if presently unhandled on a certificate.  If base is nil, the default RoundTripper is used.
 func NewIgnoreUnhandledExtensionsRoundTripper(logger hclog.Logger, base http.RoundTripper, extsToIgnore []asn1.ObjectIdentifier) http.RoundTripper {
 	if len(extsToIgnore) == 0 {
 		return base
