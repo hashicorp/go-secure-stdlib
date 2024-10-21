965a4df1d577424610a29309edab40def951db2c
Scott G. Miller <smiller@hashicorp.com>
2024-10-21 14:05:56 -0500

    Make unit test use the same bytes between methods for a direct comparison

--- cryptoutil/rsa_test.go
+++ cryptoutil/rsa_test.go
@@ -1,6 +1,7 @@
 package cryptoutil
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"github.com/stretchr/testify/require"
@@ -9,16 +10,38 @@ import (
 )
 
 type slowRand struct {
-	calls int
+	randomness  *bytes.Buffer
+	randomBytes []byte
+	calls       int
 }
+
+func newSlowRand() *slowRand {
+	r := bytes.NewBuffer(nil)
+	b := make([]byte, 2000*128)
+	rand.Read(b)
+	r.Write(b)
+	sr := &slowRand{
+		randomBytes: b,
+	}
+	sr.Reset()
+	return sr
+}
+
+func (s *slowRand) Reset() {
+	s.randomness = bytes.NewBuffer(s.randomBytes)
+}
+
+var sr = newSlowRand()
 
 func (s *slowRand) Read(p []byte) (n int, err error) {
 	// First one is free
 	if s.calls > 0 {
 		time.Sleep(50 * time.Millisecond)
 	}
+
+	n, _ = s.randomness.Read(p)
 	s.calls++
-	return rand.Read(p)
+	return
 }
 
 func TestGenerateKeyWithHMACDRBG(t *testing.T) {
@@ -28,18 +51,19 @@ func TestGenerateKeyWithHMACDRBG(t *testing.T) {
 }
 
 func BenchmarkRSAKeyGeneration(b *testing.B) {
-	var r slowRand
+	sr.Reset()
 	for i := 0; i < b.N; i++ {
-		rsa.GenerateKey(&r, 2048)
+		rsa.GenerateKey(sr, 2048)
 	}
-	b.Logf("%d calls to the RNG", r.calls)
+	b.Logf("%d calls to the RNG", sr.calls)
 }
 
 func BenchmarkRSAKeyGenerationWithDRBG(b *testing.B) {
-	var r slowRand
+	sr.Reset()
 	for i := 0; i < b.N; i++ {
-		GenerateRSAKeyWithHMACDRBG(&r, 2048)
+		sr.calls = 0
+		GenerateRSAKeyWithHMACDRBG(sr, 2048)
 	}
-	b.Logf("%d calls to the RNG", r.calls)
+	b.Logf("%d calls to the RNG", sr.calls)
 
 }
