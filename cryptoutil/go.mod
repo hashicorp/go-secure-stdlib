e1e872938ed75558ed8d2eb3de1a4d615c7ef52c
Scott Miller <sgmiller@gmail.com>
2024-11-25 12:28:01 -0600

    Add cryptoutil/rsa, RSA key generation using a seeded DRBG (#141)
    * Add an RSA key generation function which uses a DRBG to pick candidate primes rather than using a random source directly
    
    * Make unit test use the same bytes between methods for a direct comparison
    
    * wip
    
    * Add an RSA key generation function which uses a DRBG to pick candidate primes rather than using a random source directly
    
    * Make unit test use the same bytes between methods for a direct comparison
    
    * wip
    
    * formatting
    
    * typo and import fixes
    
    * typo
    
    * go 1.22
    
    * Add a max reseed count
    
    * github action update
    
    * no need to name names
    
    * fix comment inaccuracy
    
    * one more comment

---
+++ cryptoutil/go.mod
@@ -0,0 +1,14 @@
+module github.com/hashicorp/go-secure-stdlib/rsa
+
+go 1.22.0
+
+require (
+	github.com/hashicorp/go-hmac-drbg v0.0.0-20210916214228-a6e5a68489f6
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ cryptoutil/rsa_test.go
@@ -0,0 +1,85 @@
+package cryptoutil
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+type slowRand struct {
+	randomness  *bytes.Buffer
+	randomBytes []byte
+	calls       int
+}
+
+func newSlowRand() *slowRand {
+	b := make([]byte, 10247680)
+	rand.Read(b)
+	sr := &slowRand{
+		randomBytes: b,
+	}
+	sr.Reset()
+	return sr
+}
+
+func (s *slowRand) Reset() {
+	s.calls = 0
+	s.randomness = bytes.NewBuffer(s.randomBytes)
+}
+
+var sr *slowRand
+
+func TestMain(m *testing.M) {
+	sr = newSlowRand()
+	m.Run()
+}
+
+func (s *slowRand) Read(p []byte) (n int, err error) {
+	// First one is free
+	if s.calls > 0 {
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	n, _ = s.randomness.Read(p)
+	s.calls++
+	return
+}
+
+func TestGenerateKeyWithHMACDRBG(t *testing.T) {
+	key, err := GenerateRSAKeyWithHMACDRBG(rand.Reader, 2048)
+	require.NoError(t, err)
+	require.Equal(t, 2048/8, key.Size())
+	key, err = GenerateRSAKey(rand.Reader, 2048)
+	require.NoError(t, err)
+	require.Equal(t, 2048/8, key.Size())
+}
+
+func BenchmarkRSAKeyGeneration(b *testing.B) {
+	sr.Reset()
+	for i := 0; i < b.N; i++ {
+		rsa.GenerateKey(sr, 2048)
+		b.Logf("%d calls to the RNG, b.N=%d", sr.calls, b.N)
+	}
+}
+
+func BenchmarkConditionalRSAKeyGeneration(b *testing.B) {
+	platformReader = sr
+	sr.Reset()
+	for i := 0; i < b.N; i++ {
+		GenerateRSAKey(sr, 2048)
+		b.Logf("%d calls to the RNG, b.N=%d", sr.calls, b.N)
+	}
+}
+
+func BenchmarkRSAKeyGenerationWithDRBG(b *testing.B) {
+	sr.Reset()
+	for i := 0; i < b.N; i++ {
+		sr.calls = 0
+		GenerateRSAKeyWithHMACDRBG(sr, 2048)
+		b.Logf("%d calls to the RNG, b.N=%d", sr.calls, b.N)
+	}
+}
