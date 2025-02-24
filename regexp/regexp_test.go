5fe7d98bf5930b67426c469296a33d08b95759a2
Kuba Wieczorek <kuba.wieczorek@protonmail.com>
2025-02-24 18:58:07 +0000

    [VAULT-21456] Add a drop in replacement for the Go regexp package that interns regular expressions (#156)
    * Drop in replacement for the Go regexp package that does temporal caching of pattern compilation
    
    * Weak value version:  Implements a poor man's weak valued map, which captures finalization of the Regexps to clear the maps
    
    * fix locking semantics
    
    * fix locking in other func, and use the right base maps
    
    * Rename to be distinct from the original functions and thus not as subtle to later authors where used
    
    * Simplify locking, add benchmarks
    
    * remove unneeded deps
    
    * Refactor the code to use weak pointers for weak map implementation
    
    * If a weak pointer is nil, remove the entry from maps
    
    * Add regexp to the module list in the GitHub Actions go.yml workflow file, so that the tests for it are run in CI
    
    * Update the Go directive in go.mod to version 1.24, so that it's clear what stdlib version to use with this module
    
    * Swap RWMutex for a Mutex
    
    * Tidying up based on PR feedback; rename some variables to fix shadowing issues, fix returns in compile and mustComplile, edit a comment
    
    * Fix the formatting of go.yml to resolve a git conflict with main branch
    
    * Refactor mustCompile to wrap compile in a closure to reduce code duplication
    
    * Tidy up imports in regexp/regexp_test.go
    
    * Refactor compile and mustCompile to only delete the pointers from the map passed in as an argument and the reverseMap; clean up some comments
    
    * Rename cleanup to cleanupCollectedPointers and refactor it to only delete entries from either the POSIX or non-POSIX map, rather than both
    
    * Add a retry mechanism with a timeout to TestInternedRegexps
    
    * Add a test that ensures that the cleanup function does not confuse POSIX and non-POSIX regexs
    
    ---------
    
    Co-authored-by: Scott G. Miller <smiller@hashicorp.com>

---
+++ regexp/regexp_test.go
@@ -0,0 +1,181 @@
+package regexp
+
+import (
+	"regexp"
+	"runtime"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestInternedRegexps tests that the regular expressions are compiled correctly,
+// are interned, and that the cleanup of interned regexps works as expected.
+//
+// Since this test depends on the garbage collector, it is not really
+// deterministic and might flake in the future. If that happens, the calls to
+// the garbage collector and the rest of the test should be removed.
+func TestInternedRegexps(t *testing.T) {
+	testCases := map[string]struct {
+		compileFunc     func(string) (*regexp.Regexp, error)
+		mustCompileFunc func(string) *regexp.Regexp
+		mustCompile     bool
+	}{
+		"CompileInterned": {
+			compileFunc:     CompileInterned,
+			mustCompileFunc: nil,
+			mustCompile:     false,
+		},
+		"MustCompileInterned": {
+			compileFunc:     nil,
+			mustCompileFunc: MustCompileInterned,
+			mustCompile:     true,
+		},
+		"CompilePOSIXInterned": {
+			compileFunc:     CompilePOSIXInterned,
+			mustCompileFunc: nil,
+			mustCompile:     false,
+		},
+		"MustCompilePOSIXInterned": {
+			compileFunc:     nil,
+			mustCompileFunc: MustCompilePOSIXInterned,
+			mustCompile:     true,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// Compile two identical regular expressions, their pointers should be the same.
+			var r1, r2 *regexp.Regexp
+			var err error
+			pattern := ".*"
+			if tc.mustCompile {
+				r1 = tc.mustCompileFunc(pattern)
+				r2 = tc.mustCompileFunc(pattern)
+			} else {
+				r1, err = tc.compileFunc(pattern)
+				require.NoError(t, err)
+				r2, err = tc.compileFunc(pattern)
+				require.NoError(t, err)
+
+				// While we're here, check that errors work as expected.
+				_, err = tc.compileFunc("(")
+				require.Error(t, err)
+			}
+			require.True(t, r1 == r2)
+
+			// Remove references to the regexps, and run the garbage collector in a loop to see if the cleanup happens.
+			r1 = nil
+			r2 = nil
+			deadline := time.Now().Add(10 * time.Second)
+			for {
+				// Run the garbage collector twice to increase chances of the cleanup happening.
+				// This still doesn't make it deterministic, but in local testing it was enough
+				// to not flake a single time in over two million runs, so it should be good enough.
+				// A single call to runtime.GC() was flaking very frequently in local testing.
+				runtime.GC()
+				runtime.GC()
+
+				// Ensure that the cleanup happened and the maps used for interning regexp are empty.
+				l.Lock()
+				wmlen := len(weakMap)
+				rmlen := len(reverseMap)
+				l.Unlock()
+
+				if wmlen == 0 && rmlen == 0 {
+					// Cleanup happened, test can exit successfully.
+					break
+				}
+				if time.Now().After(deadline) {
+					t.Fatalf("cleanup of interned regexps did not happen in time")
+				}
+				time.Sleep(1 * time.Second)
+			}
+		})
+	}
+}
+
+// TestCleanupCorrectKind tests that the cleanup function removes the correct
+// kind (POSIX or non-POSIX) of regular expression from the correct backing maps.
+func TestCleanupCorrectKind(t *testing.T) {
+	pattern := ".*"
+	// Compile a POSIX and a non-POSIX regular expression
+	posixRegexp, err := CompilePOSIXInterned(pattern)
+	require.NoError(t, err)
+	nonPosixRegexp, err := CompileInterned(pattern)
+	require.NoError(t, err)
+
+	// Ensure they are different pointers
+	require.NotEqual(t, posixRegexp, nonPosixRegexp)
+
+	// Manually run the cleanup function for the POSIX regular expression
+	cleanupCollectedPointers(posixWeakMap[pattern], posixWeakMap)
+
+	// Ensure that the POSIX regular expression was removed from the maps
+	l.Lock()
+	require.Len(t, posixWeakMap, 0)
+	require.Len(t, reverseMap, 1)
+	require.Len(t, weakMap, 1)
+	l.Unlock()
+
+	// Compile a new POSIX regular expression with the same pattern
+	posixRegexp, err = CompilePOSIXInterned(pattern)
+	require.NoError(t, err)
+
+	l.Lock()
+	require.Len(t, posixWeakMap, 1)
+	require.Len(t, reverseMap, 2)
+	require.Len(t, weakMap, 1)
+	l.Unlock()
+
+	// Manually run the cleanup function for the non-POSIX regular expression
+	cleanupCollectedPointers(weakMap[pattern], weakMap)
+	l.Lock()
+	require.Len(t, weakMap, 0)
+	require.Len(t, reverseMap, 1)
+	require.Len(t, posixWeakMap, 1)
+	l.Unlock()
+}
+
+func BenchmarkRegexps(b *testing.B) {
+	s := make([]*regexp.Regexp, b.N)
+	for i := 0; i < b.N; i++ {
+		s[i] = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
+	}
+}
+
+func BenchmarkInternedRegexps(b *testing.B) {
+	s := make([]*regexp.Regexp, b.N)
+	for i := 0; i < b.N; i++ {
+		s[i] = MustCompileInterned(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
+	}
+}
+
+func BenchmarkConcurrentRegexps(b *testing.B) {
+	var wg sync.WaitGroup
+	for j := 0; j < runtime.NumCPU(); j++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < b.N; i++ {
+				regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)` + strconv.Itoa(i) + "-" + strconv.Itoa(j))
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkConcurrentInternedRegexps(b *testing.B) {
+	var wg sync.WaitGroup
+	for j := 0; j < runtime.NumCPU(); j++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < b.N; i++ {
+				MustCompileInterned(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)` + strconv.Itoa(i) + "-" + strconv.Itoa(j))
+			}
+		}()
+	}
+	wg.Wait()
+}
