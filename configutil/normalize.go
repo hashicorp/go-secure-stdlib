ebe38e0aa2d144cf7b888393fdc3c192bd2abb58
Danielle Miu <29378233+DanielleMiu@users.noreply.github.com>
2025-02-26 15:07:15 -0500

    add additional rfc reference in comment, use loop instead of recursion, some other typo and wording fixes in comments

--- configutil/normalize.go
+++ configutil/normalize.go
@@ -19,18 +19,19 @@ func normalizeHostPort(host string, port string, url bool) (string, error) {
 		return "", fmt.Errorf("empty hostname")
 	}
 	if ip := net.ParseIP(host); ip != nil {
-		if url && ip.To4() == nil && port == "" {
+		if url && ip.To4() == nil && ip.To16() != nil && port == "" {
 			// this is a unique case, host is ipv6 and requires brackets due to
 			// being part of a url, but they won't be added by net.JoinHostPort
 			// as there is no port
+			// See: https://www.rfc-editor.org/rfc/rfc3986#section-3.2.2
 			return "[" + ip.String() + "]", nil
 		}
 		host = ip.String()
 	} else if strings.Contains(host, ":") {
 		// host is an invalid ipv6 literal.
 		// hosts cannot contain certain reserved characters, including ":"
-		// See: https://www.rfc-editor.org/rfc/rfc3986#section-3.2.2,
-		//      https://www.rfc-editor.org/rfc/rfc3986#section-2.2
+		// See: https://www.rfc-editor.org/rfc/rfc3986#section-2.2,
+		//      https://www.rfc-editor.org/rfc/rfc3986#section-3.2.2
 		return "", fmt.Errorf("host contains an invalid IPv6 literal")
 	}
 	if port == "" {
@@ -59,19 +60,22 @@ func normalizeHostPort(host string, port string, url bool) (string, error) {
 //   - all non-host components are optional
 //
 // See:
-//   - https://www.rfc-editor.org/rfc/rfc5952#section-4
-//   - https://www.rfc-editor.org/rfc/rfc3986#section-3
+//   - https://www.rfc-editor.org/rfc/rfc5952
+//   - https://www.rfc-editor.org/rfc/rfc3986
 func NormalizeAddr(address string) (string, error) {
+	for {
+		if !strings.HasPrefix(address, "[") || !strings.HasSuffix(address, "]") {
+			break
+		}
+		address = address[1 : len(address)-1]
+	}
+
 	if address == "" {
 		return "", fmt.Errorf("empty address")
 	}
 
 	if ip := net.ParseIP(address); ip != nil {
 		return ip.String(), nil
-	}
-
-	if strings.HasPrefix(address, "[") && strings.HasSuffix(address, "]") {
-		return NormalizeAddr(address[1 : len(address)-1])
 	}
 
 	if host, port, err := net.SplitHostPort(address); err == nil {
@@ -84,9 +88,9 @@ func NormalizeAddr(address string) (string, error) {
 	// See: https://www.rfc-editor.org/rfc/rfc3986#section-3
 	if idx := strings.IndexAny(address, genDelims); idx < 0 || address[idx] != ':' {
 		const scheme = "https://"
-		// attempt to parse it as a url, we only want to try this func when we
-		// know for sure it has a scheme, since it can parse ANYTHING, but then
-		// just puts it into u.Path without the scheme
+		// attempt to parse it as a url. we only want to try this func when we
+		// know for sure it has a scheme, since it will parse ANYTHING, but
+		// just put it into u.Path when called without the scheme
 		if u, err := url.Parse(scheme + address); err == nil {
 			if u.Host, err = normalizeHostPort(u.Hostname(), u.Port(), true); err != nil {
 				return "", err
